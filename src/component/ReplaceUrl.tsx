b957f65df7283c6623fd23c2db3392ab60a956de
DongKyuK12 <jkdk981024@gmail.com>
2024-05-21 15:40:20 +0900

    fix : regix 수정

--- src/component/ReplaceUrl.tsx
+++ src/component/ReplaceUrl.tsx
@@ -57,7 +57,7 @@ export default function ReplaceUrl({ text }: ReplaceUrlProps) {
   const [linkWarningModal, setLinkWarningModal] = useState(false);
 
   const urlRegex =
-    /(?:https:\/\/)(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}(?:[-a-zA-Z0-9@:%_+.~#?&//=]*)/g;
+    /(?:https:\/\/)(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,6}(?:[-a-zA-Z0-9@:%_+.~#?&/=]*)/g;
 
   const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
     e.preventDefault();
